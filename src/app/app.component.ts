b333db2dad428f467a0d38875d3a1b7918eef353
Nikita Koynov <nikicha03@gmail.com>
2020-08-30 13:35:46 +0300

    showing loaded posts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,7 +10,7 @@ import {Post} from './post.model';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-  loadedPosts = [];
+  loadedPosts: Post[] = [];
 
   constructor(private http: HttpClient) {
   }
@@ -52,7 +52,7 @@ export class AppComponent implements OnInit {
         }
         return array;
       }))
-      .subscribe(posts => console.log(posts));
+      .subscribe(posts => this.loadedPosts = posts);
   }
 
 }
