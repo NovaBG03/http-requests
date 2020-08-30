bc40b723be72392ff19902dac3ddc879594f55e0
Nikita Koynov <nikicha03@gmail.com>
2020-08-30 13:41:18 +0300

    showing loading indicator

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,6 +11,7 @@ import {Post} from './post.model';
 })
 export class AppComponent implements OnInit {
   loadedPosts: Post[] = [];
+  isFetching = false;
 
   constructor(private http: HttpClient) {
   }
@@ -41,6 +42,7 @@ export class AppComponent implements OnInit {
   }
 
   private fetchPosts() {
+    this.isFetching = true;
     this.http
       .get<{ [key: string]: Post }>('https://ng-course-project-d091d.firebaseio.com/posts.json')
       .pipe(map(responseData => {
@@ -52,7 +54,10 @@ export class AppComponent implements OnInit {
         }
         return array;
       }))
-      .subscribe(posts => this.loadedPosts = posts);
+      .subscribe(posts => {
+        this.loadedPosts = posts;
+        this.isFetching = false;
+      });
   }
 
 }
