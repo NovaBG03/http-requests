eb1c007bf71f1f3d1c8725cc7b6e940580ae6b81
Nikita Koynov <nikicha03@gmail.com>
2020-08-30 18:08:07 +0300

    catchError operator

--- src/app/posts.service.ts
+++ src/app/posts.service.ts
@@ -1,7 +1,7 @@
 import {HttpClient} from '@angular/common/http';
 import {Injectable} from '@angular/core';
-import {Subject} from 'rxjs';
-import {map} from 'rxjs/operators';
+import {Subject, throwError} from 'rxjs';
+import {catchError, map} from 'rxjs/operators';
 
 import {Post} from './post.model';
 
@@ -40,7 +40,11 @@ export class PostsService {
           }
         }
         return array;
-      }));
+      }),
+        catchError(err => {
+          // some logic
+          return throwError(err);
+        }));
   }
 
   deletePosts() {
