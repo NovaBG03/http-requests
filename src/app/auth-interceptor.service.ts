dc5931a8b9f313a4b5490e58afc9bb2430933452
Nikita Koynov <nikicha03@gmail.com>
2020-08-30 22:25:04 +0300

    modifying httpRequest with interceptor

--- src/app/auth-interceptor.service.ts
+++ src/app/auth-interceptor.service.ts
@@ -5,7 +5,8 @@ export class AuthInterceptorService implements HttpInterceptor {
 
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     console.log('Sending request!');
-    return next.handle(req);
+    const modifiedReq = req.clone({headers: req.headers.append('Auth', 'xyz')});
+    return next.handle(modifiedReq);
   }
 
 }
